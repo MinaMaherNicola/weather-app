ad45c9cb0742c4020e2f31dfddb2c018e5bc7436
mina <minamahernicola@outlook.com>
2021-02-19 22:50:05 +0200

    Added SSL Certif

--- src/api/getWeather.js
+++ src/api/getWeather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // 1ac38f3345fcc1fb7189fa901a6b2ec1
 
 const getWeather = async (city) => {
-  const data = await axios.get('http://api.weatherstack.com/current', {
+  const data = await axios.get('https://api.weatherstack.com/current', {
     params: {
       access_key: '1ac38f3345fcc1fb7189fa901a6b2ec1',
       query: city,
